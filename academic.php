d472a60509c320f1980a9fe9b1d9b1b4dfb91987
saraizani <sarahahmadizani@gmail.com>
2016-12-05 23:48:22 +0800

    Add files via upload

---
+++ academic.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html; charset=windows-1252" http-equiv="content-type">
+    <title>academic</title>
+    <style type="text/css">
+#ACAS {  
+  font-family: Amethyst;  
+  font-size: xx-large;
+}
+
+#ss {  
+  font-family: "Times New Roman";
+}
+
+#SX {  
+  font-family: "Times New Roman";
+}
+
+#FVD {  
+  font-family: "Times New Roman";
+}
+
+#FD {  
+  font-family: "Times New Roman";
+}
+
+#GFVCD {  
+  font-family: "Times New Roman";
+}
+
+#GVFC {  
+  font-family: "Times New Roman";
+}
+
+#UUUU {  
+  font-family: "Times New Roman";
+}
+
+</style></head>
+  <body style="    background-color: #fdfbbb;">
+    <div id="ACAS" style="text-align: center;"><img style="width: 945px; height: 235px;"
+        alt="dcxs" src="education-banner.jpg"><br>
+      ACADEMIC INFO<br>
+      <br>
+      <br>
+      <table style="width: 509px; height: 137px; text-align: left; margin-left: auto; margin-right: auto;"
+        border="1">
+        <tbody>
+          <tr>
+            <td style="text-align: center;" id="ss">SUBJECTS</td>
+            <td style="text-align: center;" id="SX">TEST GRADE</td>
+            <td style="text-align: center;" id="FVD">HOMEWORK GRADE</td>
+          </tr>
+          <tr>
+            <td style="text-align: center;" id="FD">BAHASA MALAYSIA</td>
+            <td style="text-align: center;"><span style="font-family: Times New Roman;">50%</span></td>
+            <td style="text-align: center;"><span style="font-family: Times New Roman,Times,serif;">50%</span></td>
+          </tr>
+          <tr>
+            <td style="text-align: center;" id="GFVCD">ENGLISH</td>
+            <td style="text-align: center;"><span style="font-family: Times New Roman;">60%</span></td>
+            <td style="text-align: center;"><span style="font-family: Times New Roman,Times,serif;">78%</span></td>
+          </tr>
+          <tr>
+            <td style="text-align: center;" id="GVFC">MATHS</td>
+            <td style="text-align: center;"><span style="font-family: Times New Roman;">82%</span></td>
+            <td style="text-align: center;"><span style="font-family: Times New Roman,Times,serif;">94%</span></td>
+          </tr>
+          <tr>
+            <td style="text-align: center;" id="UUUU">SCIENCE</td>
+            <td style="text-align: center;"><span style="font-family: Times New Roman;">56%</span></td>
+            <td style="text-align: center;"><span style="font-family: Times New Roman;">52%</span></td>
+          </tr>
+        </tbody>
+      </table>
+      <br>
+      <br>
+      <div style="text-align: left;"><a href="EeeTee.php" class="bck">Back</a>
+        <style type="text/css">
+.bck {
+	-moz-box-shadow:inset 0px 1px 0px 0px #fceaca;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #fceaca;
+	box-shadow:inset 0px 1px 0px 0px #fceaca;
+	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #ffce79), color-stop(1, #eeaf41) );
+	background:-moz-linear-gradient( center top, #ffce79 5%, #eeaf41 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffce79', endColorstr='#eeaf41');
+	background-color:#ffce79;
+	-webkit-border-top-left-radius:15px;
+	-moz-border-radius-topleft:15px;
+	border-top-left-radius:15px;
+	-webkit-border-top-right-radius:15px;
+	-moz-border-radius-topright:15px;
+	border-top-right-radius:15px;
+	-webkit-border-bottom-right-radius:0px;
+	-moz-border-radius-bottomright:0px;
+	border-bottom-right-radius:0px;
+	-webkit-border-bottom-left-radius:0px;
+	-moz-border-radius-bottomleft:0px;
+	border-bottom-left-radius:0px;
+	text-indent:0;
+	border:1px solid #eeb44f;
+	display:inline-block;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	font-style:normal;
+	height:40px;
+	line-height:40px;
+	width:86px;
+	text-decoration:none;
+	text-align:center;
+	text-shadow:1px 1px 0px #ce8e28;
+}
+.bck:hover {
+	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #eeaf41), color-stop(1, #ffce79) );
+	background:-moz-linear-gradient( center top, #eeaf41 5%, #ffce79 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#eeaf41', endColorstr='#ffce79');
+	background-color:#eeaf41;
+}.bck:active {
+	position:relative;
+	top:1px;
+}</style> <br>
+      </div>
+      <br>
+      <div><br>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ homework.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta content="text/html; charset=windows-1252" http-equiv="content-type">
+    <title>homework</title>
+    <style type="text/css">
+#schedule {  
+  font-size: xx-large;  
+  font-family: Amethyst;
+}
+
+</style></head>
+  <body style="     background-color: #fdfbbb;">
+    <p id="schedule" style="background-color: #fdfbbb; text-align: center;"><img
+        style="width: 920px; height: 233px;" alt="fd" src="education-banner.jpg">HOmEWORK
+      LIST</p>
+    <p style="background-color: #fdfbbb; text-align: center;"><br>
+    </p>
+    <p style="background-color: #fdfbbb; text-align: left;"><span style="font-weight: bold;"><span
+          style="text-decoration: underline;">&nbsp;ENGLISH</span></span></p>
+    <p style="background-color: #fdfbbb; text-align: left;"> </p>
+    <form action="demo_form.asp" method="get"> <input name="english" value="Essay"
+        type="checkbox"> Essay homework&nbsp; <br>
+      <input name="english" value="Grammar" checked="checked" type="checkbox">
+      Grammar questions 1-5<br>
+    </form>
+    <br>
+    <span style="text-decoration: underline; font-weight: bold;">MATHS</span><br>
+    <form action="demo_form.asp" method="get"> <input name="maths" value="Subtract"
+        type="checkbox"> Subtract questions 1-5<br>
+      <input name="maths" value="Additon" checked="checked" type="checkbox">
+      Addition questions 1-5<br>
+    </form>
+    <span style="font-weight: bold; text-decoration: underline;"><br>
+      SCIENCE</span><br>
+    You have no homework pending.<br>
+    <br>
+    <br>
+    <a href="EeeTee.php" class="bck">Back</a>
+    <style type="text/css">
+.bck {
+	-moz-box-shadow:inset 0px 1px 0px 0px #fceaca;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #fceaca;
+	box-shadow:inset 0px 1px 0px 0px #fceaca;
+	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #ffce79), color-stop(1, #eeaf41) );
+	background:-moz-linear-gradient( center top, #ffce79 5%, #eeaf41 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffce79', endColorstr='#eeaf41');
+	background-color:#ffce79;
+	-webkit-border-top-left-radius:15px;
+	-moz-border-radius-topleft:15px;
+	border-top-left-radius:15px;
+	-webkit-border-top-right-radius:15px;
+	-moz-border-radius-topright:15px;
+	border-top-right-radius:15px;
+	-webkit-border-bottom-right-radius:0px;
+	-moz-border-radius-bottomright:0px;
+	border-bottom-right-radius:0px;
+	-webkit-border-bottom-left-radius:0px;
+	-moz-border-radius-bottomleft:0px;
+	border-bottom-left-radius:0px;
+	text-indent:0;
+	border:1px solid #eeb44f;
+	display:inline-block;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	font-style:normal;
+	height:40px;
+	line-height:40px;
+	width:86px;
+	text-decoration:none;
+	text-align:center;
+	text-shadow:1px 1px 0px #ce8e28;
+}
+.bck:hover {
+	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #eeaf41), color-stop(1, #ffce79) );
+	background:-moz-linear-gradient( center top, #eeaf41 5%, #ffce79 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#eeaf41', endColorstr='#ffce79');
+	background-color:#eeaf41;
+}.bck:active {
+	position:relative;
+	top:1px;
+}</style> <br>
+    <br>
+  </body>
+</html>
